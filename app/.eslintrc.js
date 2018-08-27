8f77fa982a38f1b146f95616d66329bbf2c9626f
seunzone <darealseun@gmail.com>
2018-08-27 22:47:40 +0100

    create homepage

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': 0 // for windows users
   },
   parserOptions: {
     parser: 'babel-eslint',
